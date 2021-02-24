b46e91ed6a1ef57b54c6d58c70eb04866c8a3538
guillaumepons13 <contact@guillaumepons.fr>
2021-02-24 22:05:07 +0100

    beta reco6

--- js/gmaps.js
+++ js/gmaps.js
@@ -2413,5 +2413,23 @@ if (!Array.prototype.indexOf) {
   }
 }
 
+/**
+Basic Map
+*/
+$(document).ready(function(){
+var map = new GMaps({
+  el: '#basic_map',
+  lat: 51.5073346,
+  lng: -0.1276831,
+  zoom: 12,
+  zoomControl : true,
+  zoomControlOpt: {
+      style : 'SMALL',
+      position: 'TOP_LEFT'
+  },
+  panControl : false,
+});
+});
+
 return GMaps;
 }));
