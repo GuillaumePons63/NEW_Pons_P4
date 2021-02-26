f584c38f15b1b3ef3a453e464b3e8ff641786603
guillaumepons13 <contact@guillaumepons.fr>
2021-02-26 15:34:08 +0100

    9

--- js/blocs.js
+++ js/blocs.js
@@ -140,7 +140,7 @@ function setUpLightBox() {
         targetLightbox.attr("data-gallery-id") && (o = targetLightbox.attr("data-gallery-id")), targetLightbox.attr("data-caption") || (a = "");
         var n = "";
         1 == i && (n = "autoplay");
-        var s = $('<div id="lightbox-modal" class="modal fade"><div class="modal-dialog"><div class="modal-content ' + l + ' blocs-lb-container"><button id="blocs-lightbox-close-btn" type="button" class="close-lightbox focus" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button><div class="modal-body"><a href="#" class="prev-lightbox focus" aria-label="prev" tabindex=2><span class="fa fa-chevron-left"></span></a><a href="#" class="next-lightbox class=focus" aria-label="next" tabindex=1><span class="fa fa-chevron-right"></span></a><img id="lightbox-image" class="img-responsive" src="' + e + '"><div id="lightbox-video-container" class="embed-responsive embed-responsive-16by9"><video controls ' + n + ' class="embed-responsive-item"><source id="lightbox-video" src="' + e + '" type="video/mp4"></video></div>' + a + "</div></div></div></div>");
+        var s = $('<div id="lightbox-modal" class="modal fade"><div class="modal-dialog"><div class="modal-content ' + l + ' blocs-lb-container"><button id="blocs-lightbox-close-btn" type="button" class="close-lightbox focus" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button><div class="d-flex modal-body"><a href="#" class="prev-lightbox focus" aria-label="prev" tabindex=2><span class="fa fa-chevron-left"></span></a><a href="#" class="next-lightbox class=focus" aria-label="next" tabindex=1><span class="fa fa-chevron-right"></span></a><img id="lightbox-image" class="img-responsive" src="' + e + '"><div id="lightbox-video-container" class="embed-responsive embed-responsive-16by9"><video controls ' + n + ' class="embed-responsive-item"><source id="lightbox-video" src="' + e + '" type="video/mp4"></video></div>' + a + "</div></div></div></div>");
         $("body").append(s), "fullscreen-lb" == l && ($("#lightbox-modal").addClass("fullscreen-modal").append('<a class="close-full-screen-modal animated fadeIn" style="animation-delay:0.5s;" onclick="$(\'#lightbox-modal\').modal(\'hide\');"><div class="close-icon"></div></a>'), $("#blocs-lightbox-close-btn").remove()), ".mp4" == e.substring(e.length - 4) ? ($("#lightbox-image, .lightbox-caption").hide(), $("#lightbox-video-container").show()) : ($("#lightbox-image,.lightbox-caption").show(), $("#lightbox-video-container").hide()), $("#lightbox-modal").modal("show"), "no-gallery-set" == o ? (0 == $("a[data-lightbox]").index(targetLightbox) && $(".prev-lightbox").hide(), $("a[data-lightbox]").index(targetLightbox) == $("a[data-lightbox]").length - 1 && $(".next-lightbox").hide()) : (0 == $('a[data-gallery-id="' + o + '"]').index(targetLightbox) && $(".prev-lightbox").hide(), $('a[data-gallery-id="' + o + '"]').index(targetLightbox) == $('a[data-gallery-id="' + o + '"]').length - 1 && $(".next-lightbox").hide()), addLightBoxSwipeSupport()
     }).on("hidden.bs.modal", "#lightbox-modal", function() {
         $("#lightbox-modal").remove()
@@ -173,7 +173,9 @@ function addSwipeSupport() {
 
 function addKeyBoardSupport() {
     $(window).keydown(function(t) {
-        37 == t.which ? $(".prev-lightbox").is(":visible") && $(".prev-lightbox").click() : 39 == t.which && $(".next-lightbox").is(":visible") && $(".next-lightbox").click()
+        37 == t.which ? $(".prev-lightbox").is(":visible") && $(".prev-lightbox").click() :
+        39 == t.which && $(".next-lightbox").is(":visible") && $(".next-lightbox").click() 
+        27 == t.which && $(".close-lightbox").is(":visible") && $(".close-lightbox").click()
     })
 }
 
@@ -201,3 +203,4 @@ $(document).ready(function() {
 }), $(function() {
     $('[data-toggle="tooltip"]').tooltip()
 });
+
